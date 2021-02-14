3afb5043f2dc65c74505e5ac6067885d521d3e4a
tomsasani <tomsasani1493@gmail.com>
2021-02-13 18:03:28 -0800

    file tree update

--- Rscripts/qtl_mapping_ind_muts.R
+++ Rscripts/qtl_mapping_ind_muts.R
@@ -30,8 +30,6 @@ pr <- calc_genoprob(bxd, gmap, error_prob=0.002, map_function="c-f")
 # read in the phenotype values for each BXD strain
 phen_df = read.csv(opt$phenotype_file, header=T)
 
-print (head(phen_df))
-
 phen_df = subset(phen_df, bxd_strain_conv != "BXD68_RwwJ_0462")
 
 # calculate kinship between strains using the
