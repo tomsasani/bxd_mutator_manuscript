9f70dd86359f677712035f3b689b1fd762bccb0e
tomsasani <tomsasani1493@gmail.com>
2021-10-09 10:55:39 -0600

    updates to script for local wild mouse species spectra comparison

--- py_scripts/identify_windowed_ca.wild_mice.py
+++ py_scripts/identify_windowed_ca.wild_mice.py
@@ -1,4 +1,3 @@
-import tabix
 import numpy as np
 import argparse
 import re
@@ -37,7 +36,7 @@ def count_mutations(k):
 	"""
 
     nmers = [''.join(x) for x in itertools.product('ATCG',
-                repeat=k)]
+       repeat=k)]
 
     expanded_muts = []
 
@@ -106,8 +105,11 @@ def run(args):
     # ------------
     UNK, HOM_REF, HET, HOM_ALT = range(-1, 3)
 
-    ancestor = Ancestor(args.ref, k=args.nmer,
-         sequence_always_upper=True)
+    ancestor = Ancestor(
+        args.ref,
+        k=args.nmer,
+        sequence_always_upper=True,
+    )
 
     # get a list of all possible 3-mer mutation types
     mutations = count_mutations(args.nmer)
@@ -143,131 +145,53 @@ def run(args):
     if args.exclude:
         exclude = make_interval_tree(args.exclude)
 
-    mutyh_vars = [
-        "chr4:116814337-116814338",
-        "chr4:116814394-116814394",
-        "chr4:116815657-116815658",
-        "chr4:116817415-116817416",
-        "chr4:116817418-116817419",
-        "chr4:116816475-116816476",
-    ]
-
-    var2samp = defaultdict(lambda:defaultdict())
-
-    for site in mutyh_vars:
-        vcf_hand = vcf(site)
-        for v in vcf_hand:
-            for alt_idx, alt in enumerate(v.ALT):
-                if len(alt) > 1: continue
-                ref_allele, alt_allele = v.REF, alt
-
-                # if the REF allele is longer than one nucleotide, but
-                # the REF and ALT alleles are the same length, we almost
-                # certainly need to normalize to become an SNV
-                if len(v.REF) > 1 and (len(v.REF) == len(alt)):
-                    ref_allele, alt_allele = normalize_var(v.REF, alt)
-                    if 'N' in (ref_allele, alt_allele): continue
-
-                # define the alternate genotype at this site.
-                # if this is a multiallelic site, the genotype (integer)
-                # corresponding to the first allele is 1, the genotype
-                # corresponding to the second allele is 2, and so on.
-                alt_gt = alt_idx + 1
-
-                # access sample genotypes, excluding boolean flag indicating
-                # whether sample is phased
-                gts = np.array(v.genotypes)[:,:-1]
-
-                # reformat genotypes
-                gts_reformatted = reformat_genotypes(gts, alt_gt=alt_gt)
-
-                # the index of the reference allele is always 0
-                ref_idx = 0
-
-                # access reference and alternate depths, and
-                # calculate allele balance
-                rd = v.format('AD')[:,ref_idx]
-                ad = v.format('AD')[:,alt_idx + 1]
-                td = rd + ad
-                ab = ad / td
-
-                # access genotype qualities
-                gq = v.gt_quals
-
-
-                # get a list of sample indices that meet quality thresholds
-                # e.g., non-UNK, DP>=10, GQ>=20
-                good_idxs = get_good_idxs(gts_reformatted, gq, td,
-                       min_dp = 0, min_gq = 1)
-
-                if good_idxs.shape[0] == 0: continue
-
-                for idx in idx2smp:
-                    samp = idx2smp[idx]
-
-                    if idx not in good_idxs:
-                        var2samp[site][samp] = -1
-                        continue
-                    samp_gt = gts_reformatted[idx]
-                    var2samp[site][samp] = samp_gt
-    print (var2samp)
-
-
     region_s, region_e = args.region.split(':')[-1].split('-')
     region_s, region_e = int(region_s), int(region_e)
 
-    window_size = 5e4
+    window_size = 1e5
     windows = np.arange(region_s, region_e, window_size)
 
-    oo = []
+    out_df = []
 
     for region_start in windows:
-
+        # define numpy array to store sample x mutation matrix
+        # in this window
         out_a = np.zeros((len(smp2idx), len(mut2idx)), dtype=np.int64)
-
-        region_ = "chr4:{}-{}".format(region_start, region_start + window_size)
-        vcf_h = vcf(region_)
+        window = "chr4:{}-{}".format(region_start, region_start + window_size)
+        vcf_h = vcf(window)
         for i,v in enumerate(vcf_h):
-
             # check if the current variant is in the exclude file
             if exclude and len(exclude[v.CHROM].search(v.start, v.end)) > 0: continue
             if v.var_type != "snp": continue
             if '*' in v.ALT: continue
             if len(v.REF) > 1: continue
             if len(v.ALT) > 1: continue
-            # require all samples to have a callable genotype
+            # require all samples to have a callable genotype?
             #if v.call_rate < 1: continue
 
             for alt_idx, alt in enumerate(v.ALT):
                 if len(alt) > 1: continue
-
                 ref_allele, alt_allele = v.REF, alt
-
                 # don't consider indels
                 if len(v.REF) != len(alt): continue
-
                 # if the REF allele is longer than one nucleotide, but
                 # the REF and ALT alleles are the same length, we almost
                 # certainly need to normalize to become an SNV
                 if len(v.REF) > 1 and (len(v.REF) == len(alt)):
                     ref_allele, alt_allele = normalize_var(v.REF, alt)
                     if 'N' in (ref_allele, alt_allele): continue
-
                 # define the alternate genotype at this site.
                 # if this is a multiallelic site, the genotype (integer)
                 # corresponding to the first allele is 1, the genotype
                 # corresponding to the second allele is 2, and so on.
                 alt_gt = alt_idx + 1
-
                 # access sample genotypes, excluding boolean flag indicating
                 # whether sample is phased
                 gts = np.array(v.genotypes)[:,:-1]
-
                 gts_reformatted = reformat_genotypes(gts, alt_gt=alt_gt)
 
                 # the index of the reference allele is always 0
                 ref_idx = 0
-
                 # access ref and alt depths
                 rd = v.format('AD')[:,ref_idx]
                 ad = v.format('AD')[:,alt_idx + 1]
@@ -275,17 +199,16 @@ def run(args):
                 ad[ad < 0] = 0
 
                 td = rd + ad
-                ab = ad / td
                 gq = v.gt_quals
 
                 # get a list of sample indices that meet quality thresholds
                 # e.g., non-UNK, DP>=10, GQ>=20
                 good_idxs = get_good_idxs(
-                    gts_reformatted,
-                    gq,
-                    td,
-                    min_dp=args.min_dp,
-                    min_gq=args.min_gq,
+                 gts_reformatted,
+                 gq,
+                 td,
+                 min_dp=args.min_dp,
+                 min_gq=args.min_gq,
                 )
 
                 if good_idxs.shape[0] == 0: continue
@@ -312,37 +235,39 @@ def run(args):
                     anc2gts[ii] = np.sum(anc_gts)
                     ii += 1
 
+
                 # skip this site if more than one sample in a
                 # subspecies has an ALT genotype
+                # i.e., only look at singletons
                 if np.sum(anc2gts) == 0: continue
-                #if np.sum(anc2gts) > 1: continue
-                if not np.all(anc2gts <= 1): continue
+                #if not np.all(anc2gts <= 1): continue
+                #print (str(v),)
+
+                #print (anc2gts)
+                #print (good_smps_with_mut)
 
                 out_a[good_smps_with_mut, kmer_idx] += 1
 
-
+        # loop over sample x mutation matrix and
+        # output data for this window for each sample
         for s_i in np.arange(out_a.shape[0]):
             for kmer_idx in np.arange(out_a.shape[1]):
                 samp_name = idx2smp[s_i]
                 mut_type = mutations[kmer_idx]
-
-                for site in var2samp:
-                    count = out_a[s_i, kmer_idx]
-                    total = np.sum(out_a[s_i])
-                    frac = count / total
-                    oo.append({
-                        "kmer": mut_type,
-                        "region": region_,
-                        "count": count,
-                        "frac": frac,
-                        "site": site,
-                        "sample": samp_name,
-                        "total": total,
-                        "genotype": var2samp[site][samp_name],
-                    })
+                count = out_a[s_i, kmer_idx]
+                total = np.sum(out_a[s_i])
+                frac = count / total
+                out_df.append({
+                 "kmer": mut_type,
+                 "region": window,
+                 "count": count,
+                 "frac": frac,
+                 "sample": samp_name,
+                 "total": total,
+                })
 
 
-    df = pd.DataFrame(oo)
+    df = pd.DataFrame(out_df)
 
     df.to_csv(args.out)
 
@@ -352,18 +277,18 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument('--vcf')
     p.add_argument('--ref', required=True,
-         help="path to reference genome")
+      help="path to reference genome")
     p.add_argument('--region', required=True,
-         help='region to limit analysis')
+      help='region to limit analysis')
     p.add_argument('--out', required=True,
-         help='name of output file')
+      help='name of output file')
     p.add_argument('-min_dp', default=10, type=int,
-         help='minimum depth required of singletons (default = 10)')
+      help='minimum depth required of singletons (default = 10)')
     p.add_argument('-min_gq', default=20, type=int,
-         help='minimum genotype quality required of singletons (default = 20)')
+      help='minimum genotype quality required of singletons (default = 20)')
     p.add_argument('-nmer', default=3, type=int,
-         help='length of k-mers we want to catalog (default = 3)')
+      help='length of k-mers we want to catalog (default = 3)')
     p.add_argument('-exclude',
-         help='path to BED of regions to exclude')
+      help='path to BED of regions to exclude')
     args = p.parse_args()
     run(args)
